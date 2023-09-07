b26a542d1450d72c453da697fb0e79636e171d7d
Tapiwa <tapiwachimbwanda4@gmail.com>
2023-09-07 14:47:06 -0700

    completed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-nclude<stdio.h>
+#include<stdio.h>
 /**
- *  * main function
+ *  * main - Entry point
  *   *
- *    * Returns 0 because function is of int type hence must return an integer
+ *    * Return: Always 0 (Success)
  *     */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle"\n);
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
