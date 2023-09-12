81b486186c2f8406d4df1c3d14e0a22ac975e87e
Tapiwa <tapiwachimbwanda4@gmail.com>
2023-09-12 16:06:31 -0700

    finished tasks 1-11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,10 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-
 			k = j * i;
 			if (j == 0)
 			{
-				_putchar(k + '0');
-																								}
+				_putchar(k + '0');																		}
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
