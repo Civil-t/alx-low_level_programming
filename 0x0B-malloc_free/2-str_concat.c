baf7fa979d9f75da55ae24d8a99fc05cdf9ccc18
Tapiwa <tapiwachimbwanda4@gmail.com>
2023-10-03 12:00:33 -0700

    all done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,10 +25,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (conct == NULL)
 		return (NULL);
-	i = ci = 0;								
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		conct[i] = s1[i];						
+		conct[i] = s1[i];
 		i++;
 	}
 
